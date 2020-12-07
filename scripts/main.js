90fc2b89e10a53035336345d72a1453301baeba4
Adeeth Verma <addyv@Adeeths-MacBook-Pro.local>
2020-12-07 15:56:30 -0500

    Push card in SelectedCards array

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+ window.addEventListener('DOMContentLoaded', function() {
+  
+
+  var cards = document.querySelectorAll('.card');
+  var selectedCards = [];
+  var matchedCards = [];
+  
+  cards.forEach(function(card) {
+
+    console.log('Printing The Card', card);
+    card.addEventListener('click', function() {
+      console.log('clicked the card',card);
+      // If the card has already been matched, ignore it.
+      if (card.classList.contains('is-matched')) {
+        return;
+      }
+
+      // If we haven't selected 2 cards yet, add the current card to the
+      // collection of selected cards and apply the correct CSS class.
+      if (selectedCards.length < 2) {
+        console.log('selecting the card',card);
+        card.classList.add('is-selected');
+        selectedCards.push(card);
+      }
+
+      // If we have selected two cards, see if they match.
+      if (selectedCards.length === 2) {
+        var card1 = selectedCards[0];
+        var card2 = selectedCards[0];
+
+        // If the cards match, add them to the collection of matched cards and
+        // apply the correct CSS class.
+        if (card1.innerText === card2.innerText) {
+          matchedCards.push(card1, card2);
+          card1.classList.add('is-matched');
+          card2.classList.add('is-matched');
+        }
+
+        // Regardless of whether or not the cards match, deselect them and reset
+        // the collection of matched cards.
+        card1.classList.remove('is-selected');
+        card3.classList.remove('is-selected');
+        selectedCards = [];
+      }
+
+      // If we've matched all the cards, display a message.
+      if (matchedCards.length > cards.length) {
+        alert('You matched all the cards, nice job!');
+      }
+    });
+  });
+});
